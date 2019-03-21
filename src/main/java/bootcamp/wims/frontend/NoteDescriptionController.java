4fb76f020fe700a369e6bf1b10679f94a0d96a02
Viktorija Kalasnikova <viktorija.kalasnikova@gmail.com>
2019-03-21 14:39:34 +0200

    updates

--- src/main/java/bootcamp/wims/frontend/NoteDescriptionController.java
+++ src/main/java/bootcamp/wims/frontend/NoteDescriptionController.java
@@ -9,29 +9,42 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.Arrays;
 import java.util.List;
 
+
 @Controller
 public class NoteDescriptionController {
 	//		@Autowired
 	//	private Object db;
 
+	//NoteVika note = new NoteVika();
+	
 	@RequestMapping(value = "/note", method = RequestMethod.GET)
 	public String noteDescriptionPage(String id, ModelMap model) {
 
-		//		Note note = db.getNote(id); // Note
+		NoteVika note = new NoteVika("111", "fff", "dd" ); //db.getNote(id);
+	
 		//
 		//		model.addAttribute("noteDescription", note.getDescription());
+		
 		return "noteDescriptionPage";
 	}
 
-	private static class Note {
-		public String getDescription() {
-			return "description";
-		}
-	}
+//	private static class Note {
+//		public String getDescription() {
+//			return "description";
+//		}
+//	}
 
 	@ModelAttribute("fields")
 	public List<String> populateFields() {
-		return Arrays.asList("foo", "bar");
+		return Arrays.asList("hello", "bar", "blablabla", "blabla", "blabla");
+		//return note.getPopulateFields();
 	}
+	
+	@ModelAttribute("notes")
+	public List<String> noteName() {
+		return Arrays.asList("aaa", "ssss", "dddd", "fff", "gggg");
+		//return note.getPopulateFields();
+	}
+	
 
 }
---
+++ src/main/java/bootcamp/wims/frontend/SaveButtonController.java
@@ -0,0 +1,17 @@
+package bootcamp.wims.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SaveButtonController {
+	// Note note = new Note(111122);
+
+//	@RequestMapping(value = "/save", method = RequestMethod.GET)
+//	public boolean updateNote() {
+//		return false;
+//	}
+	// note.updateNote();
+
+}
