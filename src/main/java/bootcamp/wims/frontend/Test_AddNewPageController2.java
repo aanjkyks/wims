c299e1f978178bd1e943fca02c638fa38b592745
Viktorija Kalasnikova <viktorija.kalasnikova@gmail.com>
2019-03-23 15:54:11 +0200

    edded 2 new test files

---
+++ src/main/java/bootcamp/wims/frontend/Test_AddNewPageController2.java
@@ -0,0 +1,64 @@
+package bootcamp.wims.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Controller
+public class Test_AddNewPageController2 {
+	
+	
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public ModelAndView (@RequestParam(value = "name", required = false) String name, HttpServletRequest request,
+	                       HttpServletResponse response, Model model) {
+		model.addAttribute("name", "Vika1");
+		model.addAttribute("name", "Vika2");
+		model.addAttribute("name", "Vika3");
+		model.addAttribute("name", "Vika4");
+		model.addAttribute("name", "Vika5");
+		return "Test_addNewPage2";
+	}
+	//static String enteredString;
+	
+//	@RequestMapping(value = "/add", method = RequestMethod.POST)
+////	   public static String saveInfoFromInput() {
+//	   public static void main (String[] args) {
+//	      Scanner s = new Scanner(System.in);
+//	      
+//	      System.out.print("Enter first number: " + enteredString);
+//	      
+//	      enteredString = s.nextLine();
+//	      
+//	    
+//	     // return enteredString;
+//	   }
+//	
+
+//	@RequestMapping(value = "/add", method = RequestMethod.GET)
+//    public String authenticateUser(@RequestParam("name") String name, @RequestParam("date") Date date, @RequestParam("tag") String tag, @RequestParam("description") String description, Model model) {
+//        System.out.println("coming in controller    " +name +", "+ date + ", " + tag + ", " + description);  
+//        model.addAttribute("message", "Hello Spring MVC Framework!");
+//        
+//        model.addAttribute(name, description);
+//        return "success";
+//    }
+	@RequestMapping(value = "/add_save", method = RequestMethod.POST)
+	public String homePage(@RequestParam(value = "name", required = false) String name, HttpServletRequest request,
+	                       HttpServletResponse response, Model model) {
+		model.addAttribute("name", "Vika1");
+		model.addAttribute("name", "Vika2");
+		model.addAttribute("name", "Vika3");
+		model.addAttribute("name", "Vika4");
+		model.addAttribute("name", "Vika5");
+		return "Test_addNewPage2";
+	}
+	//s
+
+}
